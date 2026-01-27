46ae7068288578435bf67b5de178420404025975
Arafat <arafatemon6969@gmail.com>
2026-01-27 10:36:36 +0600

    conditional route

--- routing-demo/app/dashboard/layout.tsx
+++ routing-demo/app/dashboard/layout.tsx
@@ -3,13 +3,16 @@ export default function ({
     users,
     revenue,
     notifications,
+    login,
 }:{
     children:React.ReactNode,
     users:React.ReactNode,
     revenue:React.ReactNode,
     notifications:React.ReactNode,
+    login:React.ReactNode,
 }){
-    return(
+    const isLoggedIn = true;
+    return isLoggedIn? (
         <>
             <div>{children}</div>
             <div style={{display:"flex"}}>
@@ -21,5 +24,7 @@ export default function ({
                 <div style={{display:"flex", flex:1}}>{notifications}</div>
             </div>
         </>
+    ): (
+        login
     )
 }
