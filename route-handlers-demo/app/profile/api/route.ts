9835c0480632736e6132f06905a95a21ad772010
Arafat <arafatemon6969@gmail.com>
2026-01-29 16:47:36 +0600

    headers, cookies & redirect

--- route-handlers-demo/app/profile/api/route.ts
+++ route-handlers-demo/app/profile/api/route.ts
@@ -1,3 +1,30 @@
-export async function GET() {
-    return new Response("Profile API data!")
+import { cookies, headers } from "next/headers"
+import { NextRequest } from "next/server"
+
+export async function GET(request: NextRequest) {
+    
+    // const headers = new Headers(request.headers)
+    // 👉 Client (browser / frontend) থেকে আসা সব HTTP headers এখানে থাকে, ei ta custom vabe headers get kora 
+    // (যেমন: Authorization, Content-Type, User-Agent)
+    //    console.log(headers.get("Authorization"))
+
+    // r ei ta build in headers
+    const headerList = await headers();
+    console.log(headerList.get("Authorization"))
+
+    // custom cookie get system 
+    // const theme = request.cookies.get("theme");
+    // console.log(theme)
+
+    // build in cookies() ei ta deye cookies get,set kora jai
+    const cookieStore = await cookies();
+    cookieStore.set("resultPerPage", "20")
+    console.log(cookieStore.get("resultPerPage"))
+ 
+    return new Response("<h1>Profile API data!</h1>",{
+        headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
+        }
+    })
 }
