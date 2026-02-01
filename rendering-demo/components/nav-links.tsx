bd10e6ca37f004b52d33e769d0d228c5601f75d2
Arafat <arafatemon6969@gmail.com>
2026-02-01 15:47:48 +0600

    client component replacement

---
+++ rendering-demo/components/nav-links.tsx
@@ -0,0 +1,10 @@
+export const NavLinks = ()=>{
+    console.log("Navlinks rendered!");
+    return(
+        <>
+            <div>
+                List of navlinks
+            </div>
+        </>
+    )
+}
---
+++ rendering-demo/components/nav-search.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { useState } from "react"
+
+export const NavSearch = ()=>{
+    const [search,setSearch] = useState();
+    console.log("NavSearch rendered!");
+    return(
+        <>
+            <div>
+                Nav search input
+            </div>
+        </>
+    )
+}
