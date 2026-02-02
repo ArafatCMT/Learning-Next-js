6bfa7ab05deb40ce70e2a9174298d09c7f99b2f7
Arafat <arafatemon6969@gmail.com>
2026-02-02 12:22:55 +0600

    data fetching in server components

---
+++ data-fetching-demo/app/users-server/page.tsx
@@ -0,0 +1,40 @@
+type users = {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    phone: string
+}
+
+export default async function UsersServer() {
+    await new Promise ((resolve) => setTimeout(resolve,2000))
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    if (response.ok === false){
+        throw new Error("Faild to fatch users!")
+    }
+    const users: users[] = await response.json();
+    // console.log(users)
+    return (
+        <>
+            <ul className="space-y-4 p-4">
+                {users.map((user) => {
+                    return (
+                        <li key={user.id} className="p-4 bg-white shadow-md rounded-lg text-gray-700">
+                            <div className="font-bold">{user.name}</div>
+                            <div className="text-sm">
+                                <div>Username: {user.username}</div>
+                                <div>Email: {user.email}</div>
+                                <div>Phone: {user.phone}</div>
+                            </div>
+
+
+                        </li>
+                    )
+                })
+
+                }
+            </ul>
+        </>
+    )
+}
